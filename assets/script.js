05601a4a4610ca9ae4967d79a1a8c3d20d38bab1
Kristin Evans <kevans0625@gmail.com>
2020-01-12 21:43:33 -0600

    Created script and added in variables for pswd characteristics.

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', function() {
+  var length = +prompt("Please enter a character length between 8-128.");
+
+  if (length > 8 && length < 128) {
+    var upper = confirm("Should your password include uppercase letters?");
+    var lower = confirm("Should your password include lowercase letters?");
+    var number = confirm("Should your password include numbers?");
+    var symbols = confirm("Should your password include special characters?");
+
+    if  ((upper != true || lower != true|| number != true || symbols != true)) {
+      alert("You must select at least one character type!");
+  
+      upper = confirm("Should your password include uppercase letters?");
+      lower = confirm("Should your password include lowercase letters?");
+      number = confirm("Should your password include numbers?");
+      symbols = confirm("Should your password include special characters?");
+    }
+  }
+  else{
+
+    length = prompt("Length must be 8-128 characters. How many characters would you like your password to be?");
+    upper = confirm("Should your password include uppercase letters?");
+    lower = confirm("Should your password include lowercase letters?");
+    number = confirm("Should your password include numbers?");
+    symbols = confirm("Should your password include special characters?");
+  } 
+  
+  
